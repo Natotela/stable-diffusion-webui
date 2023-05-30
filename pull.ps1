22dfd764ded17fd50026d03caafd9d549525181b
Natotela <labthyneighbor@gmail.com>
2023-05-30 11:41:53 +0300

    %DATE% %TIME% GitPolled

--- pull.ps1
+++ pull.ps1
@@ -3,4 +3,5 @@ $custNodes = "F:\Progz\stable-diffusion-webui\extensions\"
 $subfolders = Get-ChildItem -Path $custNodes -Directory
 cd $custNodes
 ForEach ($subfolder in $subfolders) { Write-Host $subfolder.FullName ; cd $subfolder ; git pull; cd.. }
+# ForEach ($subfolder in $subfolders) { Write-Host $subfolder.FullName ; cd $subfolder ; git add -A ; git commit -m "%%DATE%% %%TIME%% lamalo" ; git pull; cd.. }
 cd $backto
